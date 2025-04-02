8cd9c52200c3fadbd8f1231481e9d97e3dcb0a2a
Andrew Taylor <ataylor370@gatech.edu>
2025-04-02 15:16:01 -0400

    Base file

--- Money_Parse/views.py
+++ Money_Parse/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+def home(request):
+     return render(request,'about.html',{})
+def about(request):
+     return render(request, 'about.html',{}) #brackets at the end allow us to pass in stuff during render
 # Create your views here.
