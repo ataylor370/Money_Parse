3342ed9877f2b6a27d31b05f7d65a9733191744f
Andrew Taylor <ataylor370@gatech.edu>
2025-04-02 16:00:21 -0400

    Base file

--- CS2340_Project_2/urls.py
+++ CS2340_Project_2/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('Money_parse.urls')),
     path('admin/', admin.site.urls),
     path('about/',include('Money_Parse.urls'))
 ]
